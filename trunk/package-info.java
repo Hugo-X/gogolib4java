2e7c9dadfb6195f5115916e0f476aff517085084
hongyu.xiong@gmail.com <hongyu.xiong@gmail.com@bdf1fa0f-49a4-cffb-f570-b9be8599d3aa>
2011-07-16 15:26:36 +0000

    update the package-info

--- trunk/package-info.java
+++ trunk/package-info.java
@@ -6,6 +6,6 @@ So, with GoGo Monitor 3.4.x you cannot connect to the GoGo Server again after yo
 GoGo Monitor 3.5.0 is going to fix this problem. You may need the latest <a href="http://www.gogoboard.org">GoGo monitor</a><br/>
 @version  2011.06
 @author <i>Hongyu Xiong</i> <a href="mailto:hongyu.xiong@gmail.com">hongyu.xiong@gmail.com</a>
-@see <a href="http://cn.scnu.ac.kr/~xiong/abm/gogolib">GoGo Lib Project at cn.scnu.ac.kr</a>
+@see <a href="http://code.google.com/p/gogolib4java">GoGo Lib Project at cn.scnu.ac.kr</a>
  */
 package kr.ac.scnu.cn.gogolib;

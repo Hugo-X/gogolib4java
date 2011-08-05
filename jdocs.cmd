8e8ec5d5e1b99e578454c3fd3102fac6413d7f81
hongyu.xiong@gmail.com <hongyu.xiong@gmail.com@bdf1fa0f-49a4-cffb-f570-b9be8599d3aa>
2011-08-05 02:36:10 +0000

    modify project configuration create ant scripts for doc and jar generation

--- jdocs.cmd
+++
@@ -1,5 +0,0 @@
-@set JDK_BIN=C:\Java\jdk1.6.0_24\bin
-@set path=%path%;%JDK_BIN%
-
-javadoc -author -version -d docs -package -windowtitle "gogolib API Specification"  GoGoClient.java GoGoSessionManager.java package-info.java
-pause
